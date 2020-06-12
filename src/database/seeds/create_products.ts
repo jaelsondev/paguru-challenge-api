4c5ad13e951c6752cfe198b65b88d2fc295dfcc2
Jaelson Santos <jaelson.jua@gmail.com>
2020-06-12 05:28:40 -0300

    Adicionando cloudnary 2

--- src/database/seeds/create_products.ts
+++ src/database/seeds/create_products.ts
@@ -30,5 +30,33 @@ export async function seed(knex: Knex) {
         "https://res.cloudinary.com/jaelsondev/image/upload/v1591948967/uploads/tie-dye_l7zxqh.png",
       price: 50.55,
     },
+    {
+      name: "Concha Marked",
+      // image: "tie-dye",
+      image:
+        "https://res.cloudinary.com/jaelsondev/image/upload/v1591950248/uploads/Concha_marcada_xqmows.png",
+      price: 102.55,
+    },
+    {
+      name: "Concha Shaker",
+      // image: "tie-dye",
+      image:
+        "https://res.cloudinary.com/jaelsondev/image/upload/v1591950249/uploads/Concha_abanador_q3blhc.png",
+      price: 67.40,
+    },
+    {
+      name: "Concha Worm",
+      // image: "tie-dye",
+      image:
+        "https://res.cloudinary.com/jaelsondev/image/upload/v1591950249/uploads/Concha_Minhoca_stvf7s.png",
+      price: 29.90,
+    },
+    {
+      name: "Concha Beautiful",
+      // image: "tie-dye",
+      image:
+        "https://res.cloudinary.com/jaelsondev/image/upload/v1591950248/uploads/Concha_carrascuda_pryooc.png",
+      price: 299.90,
+    },
   ]);
 }
